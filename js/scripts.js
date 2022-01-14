eec8692c6ba4932b7ef4b4cd1ca82a036dbbbd24
hmcvay <hayleymcvay8@gmail.com>
2022-01-14 10:41:45 -0800

    Apply jQuery to call functions on hidden divs.

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,23 @@
 $(document).ready(function() {
-  $("button#submitButton").click(function(Event){
+  $("button#submitButton").click(function(event){
+    const answer1 = parseInt($("select#firstQuestion").val());
+    const answer2 = parseInt($("select#secondQuestion").val());
+    const answer3 = parseInt($("select#thirdQuestion").val());
+    const answer4 = parseInt($("select#fourthQuestion").val());
+    const answer5 = parseInt($("select#fifthQuestion").val());
+
+    const total = answer1 + answer2 + answer3 + answer4 + answer5;
+
+    if (total < 6) {
+      $("#result1").show();
+    } else if (total > 6){
+      $("#result2").show();
+    }
+    if (total === 12) {
+      $("#result3").show();
+    }
     
-  })
-})
+
+  }
+)})
 
