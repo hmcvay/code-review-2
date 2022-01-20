e6e90f08c044d700e4f2ff561c67b4c556b5294e
hmcvay <hayleymcvay8@gmail.com>
2022-01-20 07:16:25 -0800

    FINAL EDIT: Fix typo on scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -13,9 +13,9 @@ $(document).ready(function() {
       $("#result2").hide();
       $("#result1").show();
     } else if (total === 12) {
-        $("#result1").hide();
-        $("result3").hide();
-        $("#result2").show();
+      $("#result1").hide();
+      $("result3").hide();
+      $("#result2").show();
     } else {
       $("#result1").hide();
       $("#result2").hide();
